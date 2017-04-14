7e21cf135beca0176d4862dc09993b368e8f9e57
sambadiallo <samba.diallo@outlook.be>
2017-04-14 12:02:05 +0200

    KonKon-Mobile:
    - Creating mobile structure

---
+++ KonKon.Mobile/KonKon.Mobile/KonKon.Mobile/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DryIoc;
+using Prism.DryIoc;
+using KonKon.Mobile.Views;
+using Xamarin.Forms;
+
+namespace KonKon.Mobile
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello from Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            NavigationRegistation();
+        }
+
+        private void NavigationRegistation()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
