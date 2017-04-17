693a9ededea89cf5f44ab733dc20968c8fc9a204
sambadiallo <samba.diallo@outlook.be>
2017-04-17 17:21:49 +0200

    KONKON-APP: app structure creating

---
+++ KonKon.App/KonKon.App/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace KonKon.App.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
